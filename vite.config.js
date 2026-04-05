762cae2e897f2d414f8908b2cacdf5410ffbbe79
Moadel <moadel17@gmail.com>
2026-04-05 03:28:21 +0200

    Rebuild project after vite base fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Coffee-Brand/", // مهم جدًا للنشر على GH Pages
+  base: "./", // ← مهم جدًا للنشر على Vercel
   plugins: [react()],
 });
